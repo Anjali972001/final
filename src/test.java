3b0bc5cc118f283c36ac7d97e93fda099328bd88
Anjali <anjalikumari972001@gmail.com>
2021-11-15 21:13:23 +0530

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        final int a= 10;
+        i=i+20;
+        System.out.println(i);
+    }
+}
+/*  final keyword with variable --- variable will become constant, and we cannot change the value.
+     output---> error */
